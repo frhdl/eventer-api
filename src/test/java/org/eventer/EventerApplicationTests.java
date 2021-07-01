087f136b15dc6ce5be4d1e492cfbb4b56ecdeadf
Francisco Diniz <francisco.diniz@bolttech.io>
2021-07-01 22:35:17 +0200

    basic project

---
+++ src/test/java/org/eventer/EventerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.eventer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
